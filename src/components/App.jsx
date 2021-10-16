6b09340388ea72ba43653bb7363a566fa33ca457
sagnik369 <sagnik369@gmail.com>
2021-10-16 14:05:07 +0530

    chnages in the random word made

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,36 +6,40 @@ import Videodetails from "./Videodetails";
 import Videolist from "./Videolist";
 import Footer from "./Footer";
 import YTSearch from "youtube-api-search";
+import request from "sync-request";
 
 const API_KEY = "AIzaSyDA_9NjW8fDDyMcft-ZxZXZNKJwg3hpaHc";
 
+const res = request("GET", "https://random-word-api.herokuapp.com/word");
+var ranWord = res.getBody().slice(2, -2);
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.videoSearch(ranWord);
+    this.state = { videos: [], selectedVideo: null };
+  }
 
-    constructor(props) {
-        
-        super(props);
-        this.videoSearch('Oh My Girl');
-        this.state = { videos: [], selectedVideo: null };
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) =>
+      this.setState({ videos: videos, selectedVideo: videos[0] })
+    );
+  }
 
-    }
-
-    videoSearch(term) {
-
-        YTSearch({ key: API_KEY, term: term }, (videos) => this.setState({ videos: videos, selectedVideo: videos[0] }));
-    }
-
-    render() {
-        return (
-            <div>
-                <Jumbo />
-                <Searchbar onSearchChange={(term) => this.videoSearch(term)} />
-                <Videodetails video={this.state.selectedVideo} />
-                <Videolist onSelectedVideo={(selectedVideo) => this.setState({ selectedVideo })} videos={this.state.videos} />
-                <Footer />
-            </div>
-        );
-    }
-
-};
+  render() {
+    return (
+      <div>
+        <Jumbo />
+        <Searchbar onSearchChange={(term) => this.videoSearch(term)} />
+        <Videodetails video={this.state.selectedVideo} />
+        <Videolist
+          onSelectedVideo={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Jumbo.jsx
+++ src/components/Jumbo.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 function Jumbo() {
-    return (
+  return (
     <div id="navi" className="jumbotron">
-        <div id="forflex"><img id="logo" src="https://img.icons8.com/nolan/64/react-native.png" alt="HyperLoop" />
-        <h1 id="display" className="display-1 px-4">HyperLoop</h1></div><hr className="my-4"/>
+      <div id="forflex">
+        <img
+          id="logo"
+          src="https://img.icons8.com/nolan/64/react-native.png"
+          alt="HyperLoop"
+        />
+        <p id="display" className="display-1 px-4">
+          Will be down for further development
+        </p>
+      </div>
+      <hr className="my-4" />
     </div>
-    );
+  );
 }
 
 export default Jumbo;
